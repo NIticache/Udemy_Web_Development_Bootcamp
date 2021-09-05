e1129cd9e84b2b4fea740b2b66047e7b03c74caa
NITEESH <niteeshvijayakumar500@gmail.com>
2021-09-05 10:21:34 +0530

    Notes app

---
+++ React -Keeper_App/keeper-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Done by Niteesh</p>
+    </footer>
+  );
+}
+
+export default Footer;
